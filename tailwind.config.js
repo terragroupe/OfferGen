3bc1e589e4f3c500c5a2bda4bc40a968b1a5946b
fahim2204 <fahimfaisal1998@gmail.com>
2023-02-25 21:18:12 +0600

    Index Page: Color, Font Done ✔

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
     // "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'barlow': ['Barlow']
+      },
+      colors: {
+        'tgbrown': {
+          '50': '#f6f4f0',
+          '100': '#eae3d7',
+          '200': '#d6c9b2',
+          '300': '#bea786',
+          '400': '#a6845b',
+          '500': '#9c7956',
+          '600': '#866248',
+          '700': '#6c4c3c',
+          '800': '#5c4237',
+          '900': '#503a33',
+        },
+      }
+    },
   },
   plugins: [],
 };
